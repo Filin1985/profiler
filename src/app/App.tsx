2e5fd4452431ca2af23c618d485ef3777889a243
Marat Ikhsanov <ihsmen@gmail.com>
2024-10-14 20:55:28 +0300

    Create route config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import React, {Suspense, useContext, useState} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
+import React from 'react';
+import {Link} from 'react-router-dom';
 import './styles/index.scss'
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
 import {classNames} from "shared/lib/classNames/classNames";
+import {AppRouter} from "app/providers/router";
 
 function App() {
     const {theme, toggleTheme} = useTheme();
@@ -14,12 +13,7 @@ function App() {
             <button onClick={toggleTheme}>Change theme</button>
             <Link to={'/'}>Главная страница</Link>
             <Link to={'/about'}>Про страница</Link>
-            <Suspense fallback={<div>Loading....</div>}>
-                <Routes>
-                    <Route path="/" element={<MainPage/>}/>
-                    <Route path="/about" element={<AboutPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter/>
         </div>
     );
 }
