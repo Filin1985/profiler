7da14c2f99bad3dc459fe102e4c8deb23c218fee
Marat Ikhsanov <ihsmen@gmail.com>
2025-08-22 07:26:58 +0300

    Add loki

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -10,23 +10,29 @@ export default ({ config }: {config: webpack.Configuration}) => {
         entry: '',
         src: path.resolve(__dirname, '..', '..', 'src'),
     };
+    config.resolve = config.resolve || {};
+    config.resolve.modules = config.resolve.modules || [];
     config.resolve.modules.push(paths.src);
+    config.resolve.extensions = config.resolve.extensions || [];
     config.resolve.extensions.push('.ts', '.tsx');
 
     // eslint-disable-next-line no-param-reassign
-    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
-        if (/svg/.test(rule.test as string)) {
-            return { ...rule, exclude: /\.svg$/i };
-        }
+    config.module = config.module || { rules: [] };
+    config.module.rules = (config.module.rules || [])
+        .filter((rule): rule is RuleSetRule => typeof rule === 'object' && rule !== null)
+        .map((rule) => {
+            if (/svg/.test(rule.test as string)) {
+                return { ...rule, exclude: /\.svg$/i };
+            }
 
-        return rule;
-    });
+            return rule;
+        });
 
-    config.module.rules.push({
+    config?.module?.rules.push({
         test: /\.svg$/,
         use: ['@svgr/webpack'],
     });
-    config.module.rules.push(buildCssLoader(true));
+    config?.module?.rules.push(buildCssLoader(true));
 
     return config;
 };
